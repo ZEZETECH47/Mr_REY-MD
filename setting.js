45e6b76e21803a1fec9e07859d7d230b5b2b5d1b
ZEZETECH47 <humphreymbise75@gmail.com>
2025-09-23 00:10:07 +0300

    Update and rename command.js to setting.js

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Rey Bot',
+  author: '‎',
+  botName: "REY-AI Bot",
+  botOwner: 'Professor', // Your name
+  ownerNumber: '255747397675', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.1.9",
+  updateZipUrl: "https://github.com/ZEZETECH47/Mr_REY-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
